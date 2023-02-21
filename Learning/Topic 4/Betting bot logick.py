9085d44a3f45e6f5c2a4389fd976a790edba603c
nikitaigolkin <nigolkin97@gmail.com>
2023-02-21 16:55:13 +0600

    New intenger 14

--- Learning/Topic 4/Betting bot logick.py
+++ Learning/Topic 4/Betting bot logick.py
@@ -1,13 +1,1 @@
-def say_hello(name, city, state):
-    if len(name) <= 2:
-        return "Hello, " + str(name[0]) + " " + str(name[1]) + "! " + "Welcome to " + city + ", " + state + "!"
-    else:
-        return "Hello, " + str(name[0]) + " " + str(name[1]) + str(
-            name[2]) + "! " + "Welcome to " + city + ", " + state + "!"
-
-
-say_hello(['John', 'Smith'], 'Phoenix', 'Arizona')
-
-121
-91
-2
+def str_count(strng, letter):
