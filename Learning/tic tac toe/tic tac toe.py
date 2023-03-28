e1c4316b443c00583be1efb52a84dbb9ead881ac
nikitaigolkin <nigolkin97@gmail.com>
2023-03-28 21:40:40 +0600

    New intenger 14

--- Learning/tic tac toe/tic tac toe.py
+++ Learning/tic tac toe/tic tac toe.py
@@ -4,24 +4,42 @@ def display_board(board):
     print(board[4] + ' | ' + board[5] + ' | ' + board[6])
     print('- | - | -')
     print(board[1] + ' | ' + board[2] + ' | ' + board[3])
-    print('\n' * 100)
 
 
-display_board(['#', 'X', 'o', 'X', 'o', 'x', 'o', 'x', 'o', '×'])
+test_board = [" "] * 10
+display_board(test_board)
 
 
 def player_input():
-    player_1 = int(input("Print 1 if you wanna play x and 0 for play 0"'\n'"player 1 ="))
-    if player_1 != 1 or player_1 != 0:
-        print("Please write the number for choose side")
-    elif player_1 == 1:
-        return 1
+    player_1 = input("choose the side X or O"'\n')
+    answers = set(["X", "x", "O", "o"])
+    zero = set(["O", "o"])
+    while player_1 not in answers:
+        print(input("Please choose side"'\n'))
+    if player_1 not in zero:
+        return "x"
     else:
-        return 2
-    if player_1 == 1:
-        player_2 = 2
-    elif player_1 == 0:
-        player_2 = 1
+        return "o"
 
 
-player_input()
+def place_marker(board):
+    if player_input() == "x":
+        marker = "x"
+    else:
+        marker = "o"
+    position = int(input("Position ------"))
+    if marker.upper() == "X":
+        board[position] = marker
+    else:
+        board[position] = marker
+
+
+place_marker(test_board)
+display_board(test_board)
+
+
+def win_check(board, mark):
+
+
+
+win_check(test_board, )
